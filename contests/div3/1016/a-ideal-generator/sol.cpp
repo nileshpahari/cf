54530d3d3fc4f071122362b9e8cef325151f89ad
Nilesh Pahari <nileshpahari@protonmail.com>
2025-04-12 19:32:06 +0530

    add solns

---
+++ contests/div3/1016/a-ideal-generator/sol.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    if (n % 2 == 1)
+      cout << "YES\n";
+    else
+      cout << "NO\n";
+  }
+  return 0;
+}
