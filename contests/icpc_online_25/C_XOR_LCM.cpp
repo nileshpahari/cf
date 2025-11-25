e5c879a71d706ca726293b345e22d9a13cb2a646
Nilesh Pahari <nileshpahari@protonmail.com>
2025-11-26 00:20:13 +0530

    update state

---
+++ contests/icpc_online_25/C_XOR_LCM.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                                                               \
+  ios::sync_with_stdio(false);                                                 \
+  cin.tie(NULL);
+
+#define nl '\n'
+
+void solve() {
+  long long c;
+  cin >> c;
+  cout << c << " " << (c << 24) << nl;
+}
+
+int main() {
+  fastio();
+  int t = 1;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+  return 0;
+}
